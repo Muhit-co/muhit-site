ab6d86526efada71fc694360033e64bb56f206cd
Daniel Swakman <dswakman@oefenweb.nl>
2015-03-26 22:21:42 +0200

    added multi-language support (&htaccess)

--- site/config/config.php
+++ site/config/config.php
@@ -28,3 +28,19 @@ make Kirby work. For more fine-grained configuration
 of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
+
+c::set('languages', array(
+  array(
+    'code'    => 'tr',
+    'name'    => 'Türkçe',
+    'default' => true,
+    'locale'  => 'tr_TR',
+    'url'     => '/',
+  ),
+  array(
+    'code'    => 'en',
+    'name'    => 'English',
+    'locale'  => 'en_US',
+    'url'     => '/en',
+  ),
+));
