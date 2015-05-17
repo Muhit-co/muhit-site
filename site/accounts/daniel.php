c06333caa5e96301380adb97a8d36574a1b5301b
Daniel Swakman <dswakman@oefenweb.nl>
2015-05-17 19:34:30 +0300

    added mailchimp sign-up form functionality

--- site/accounts/daniel.php
+++ site/accounts/daniel.php
@@ -8,9 +8,10 @@ password: >
   $2a$10$We8M703kf/V0GIXX4cIMt.qN7Rjs/Vc54t57QH4epkeEfXw1.F5Zu
 language: en
 role: admin
-token: df4ec3fd6fe9c52a616af708a8088439bc712e23
+token: 5184451d2f35b0d282e1d0cd54eaefb5ef20dd6a
 history:
+  - iletisim
+  - hakkimizda
   - giris
   - muhit-nedir
   - ozellikler
-  - hakkimizda
---
+++ site/snippets/mailchimp_form.php
@@ -0,0 +1,20 @@
+<div id="mc_embed_signup">
+  <form action="//muhit.us10.list-manage.com/subscribe/post?u=6a8d1b17307f12faebaebd16e&amp;id=95f5915f21" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+
+      <div id="mce-responses" class="clear">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none"></div>
+      </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+
+      <div style="position: absolute; left: -5000px;"><input type="text" name="b_6a8d1b17307f12faebaebd16e_95f5915f21" tabindex="-1" value=""></div>
+
+      <div class="field-group">
+        <label for="mce-EMAIL"></label>
+        <input type="email" value="" name="EMAIL" class="required email u-mr10" id="mce-EMAIL" placeholder="Email Address">
+        <input type="submit" value="&#62147;" name="subscribe" id="mc-embedded-subscribe" class="btn">
+      </div>
+
+    </div>
+  </form>
+</div>
