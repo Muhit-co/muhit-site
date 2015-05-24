11d16487028b5bbdcd4521c818932f8ed8293bc5
Guney Can Gokoglu <guneycan@gmail.com>
2015-05-24 14:46:33 +0300

    define a root dir and then include relative

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 
 define('DS', DIRECTORY_SEPARATOR);
+if (!defined('ROOT')) {
+	define('ROOT', __DIR__);
+}
 
 // load kirby
-require(__DIR__ . DS . 'kirby' . DS . 'bootstrap.php');
+require __DIR__ . DS . 'kirby' . DS . 'bootstrap.php';
 
 // check for a custom site.php
-if(file_exists(__DIR__ . DS . 'site.php')) {
-  require(__DIR__ . DS . 'site.php');
+if (file_exists(__DIR__ . DS . 'site.php')) {
+	require __DIR__ . DS . 'site.php';
 } else {
-  $kirby = kirby();
+	$kirby = kirby();
 }
 
 // render
